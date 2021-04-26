ac98e970fd35391ca8179e21cd6d2d3ad3210970
Zakaria <zakariaharrane2@gmail.com>
2021-04-26 02:13:36 +0100

    Table done with sorting and pagination

--- src/pages/employees/Employees.js
+++ src/pages/employees/Employees.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageHeader from '../../components/PageHeader';
 import EmployeeForm from './EmployeeForm';
 import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
-import { makeStyles, Paper } from '@material-ui/core';
+import {
+  makeStyles,
+  Paper,
+  TableBody,
+  TableCell,
+  TableRow,
+} from '@material-ui/core';
+import useTable from '../../components/useTable';
+import * as EmployeesService from '../../services/EmployeeService';
 
+const headCells = [
+  {
+    id: 'fullname',
+    label: 'Employee full name',
+  },
+  {
+    id: 'email',
+    label: 'Email address',
+  },
+  {
+    id: 'mobile',
+    label: 'Mobile number',
+  },
+  {
+    id: 'department',
+    label: 'Department',
+  },
+];
 const Employees = () => {
+  const [records, setRecords] = useState(EmployeesService.getAllEmployees());
+  console.log('zaza: ' + records);
   const classes = useStyles();
+  const {
+    TblContainer,
+    TblHead,
+    TblPagination,
+    recordsAfterPagingAndSorting,
+  } = useTable(records, headCells);
   return (
     <>
       <PageHeader
@@ -15,6 +49,20 @@ const Employees = () => {
       />
       <Paper className={classes.pageContent}>
         <EmployeeForm />
+        <TblContainer>
+          <TblHead />
+          <TableBody>
+            {recordsAfterPagingAndSorting().map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.fullname}</TableCell>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>{item.mobile}</TableCell>
+                <TableCell>{item.department}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TblContainer>
+        <TblPagination />
       </Paper>
     </>
   );
